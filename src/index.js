21ce8b3a33cfbe6cb98c8c15a3f4d9858a40f595
Austine Juma <austinejumah001@gmail.com>
2026-03-12 11:02:14 +0300

    feat: add click handler to nav buttons and render homepage on page load

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import "./styles.css";
+import { homePageComponent } from "./homePage.js";
+import { menuComponent } from "./menu.js";
+import { aboutComponent } from "./about.js";
+import { clearContent, render, setState } from "./utils.js";
+
+const nav = document.querySelector("nav");
+const home = document.querySelector("#home");
+
+nav.addEventListener("click", (e) => {
+    let buttonId = e.target.id;
+
+    switch(buttonId){
+        case "home":
+            clearContent();
+            render(homePageComponent);
+            setState(buttonId)
+            break;
+        case "menu":
+            clearContent();
+            render(menuComponent);
+            setState(buttonId)
+            break;
+        case "about":
+            clearContent();
+            render(aboutComponent);
+            setState(buttonId)
+    }
+})
+
+home.click();
